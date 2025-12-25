64faf2bdc65e5a1c1bd914f297b739c8192e8d6e
durukula <22070001013@stu.yasar.edu.tr>
2025-12-25 15:57:34 +0300

    Add root endpoint for health check

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("App is running");
+});
+
+
 app.get("/hello", (req, res) => {
   res.json({
     message: "Hello from Node.js app!",
